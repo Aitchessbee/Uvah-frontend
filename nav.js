45cf3a145a8126610b56acb82612f8fe8fc8dfff
Aitchessbee <bediharsiddak@gmail.com>
2022-02-26 22:01:13 +0530

    Added Roadmap template

---
+++ nav.js
@@ -0,0 +1,8 @@
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
